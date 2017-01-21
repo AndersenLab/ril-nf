1b4c0087101562ce6f2349e7194acb5091f20ec1
Daniel Cook <danielecook@gmail.com>
2017-01-21 15:52:32 -0600

    Fix gt out

--- plot_hmm.R
+++ plot_hmm.R
@@ -3,7 +3,7 @@ library(tidyverse)
 df <- readr::read_tsv("gt_hmm_fill.tsv") %>%
   dplyr::group_by(sample) %>%
   dplyr::filter(chrom != "MtDNA") %>%
-  dplyr::mutate(gt = ifelse(gt == 1, "CB4856", "N2"))
+  dplyr::mutate(gt = ifelse(gt == 1, "N2", "CB4856"))
 
 df$index <- dplyr::group_indices(df)
 
@@ -12,7 +12,7 @@ names(strain_index) <- df$index + 0.5
 
 ggplot(df) +
   geom_rect(aes(xmin = start, xmax = end, ymin = index, ymax = index + 1, fill = gt)) +
-  scale_fill_manual(values = c("#0080FF", "#FF8000")) +
+  scale_fill_manual(values = c("#0080FF","#FF8000")) +
   facet_grid(.~chrom, scales="free", space="free") +
   theme_bw() +
   scale_x_continuous(labels = function(x) { x/1e6 }, expand = c(0,0)) +
